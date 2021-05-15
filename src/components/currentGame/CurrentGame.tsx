b2fcfd66bf2bc37994d0ea7059e027f76ece8377
ctabares06 <ctr9120@gmail.com>
2021-05-15 01:19:38 -0500

    component currentGame refactor sccs, button disabled color

--- src/components/currentGame/CurrentGame.tsx
+++ src/components/currentGame/CurrentGame.tsx
@@ -9,22 +9,20 @@ const CurrentGame: React.FC = () => {
   const { curriculums, hired, fired } = useContext(CurriculumsContext)!.states;
 
   return (
-    <section className="resume card card--rounded card--white card--shadow">
+    <section className="resume">
         <h3 className="resume__title">Resume</h3>
-        <div className="resume__body">
-          <p className="resume__body__item">
-            <span className="resume__body__item__title">Curriculums:</span> 
+          <p className="resume__item">
+            <span className="resume__item__title">Curriculums:</span> 
             <span>{curriculums.length}</span> 
           </p>
-          <p className="resume__body__item">
-            <span className="resume__body__item__title">Hired:</span> 
-            <span className={ "resume__body__item__value "+(hired.length === maxHired ? "resume__body__item__value--green" : "")}>{hired.length}/{maxHired}</span>
+          <p className="resume__item">
+            <span className="resume__item__title">Hired:</span> 
+            <span className={ "resume__item__value "+(hired.length === maxHired ? "resume__item__value--green" : "")}>{hired.length}/{maxHired}</span>
           </p>
-          <p className="resume__body__item">
-            <span className="resume__body__item__title">Fired:</span> 
-            <span className={ "resume__body__item__value "+(fired.length === maxFired ? "resume__body__item__value--red" : "")}>{fired.length}/{maxFired}</span>
+          <p className="resume__item">
+            <span className="resume__item__title">Fired:</span> 
+            <span className={ "resume__item__value "+(fired.length === maxFired ? "resume__item__value--red" : "")}>{fired.length}/{maxFired}</span>
           </p>
-        </div>
     </section>
   )
 };
