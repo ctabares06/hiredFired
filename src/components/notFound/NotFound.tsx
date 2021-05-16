e17ee5e9369043efb2ce1b87852664248674cc96
ctabares06 <ctr9120@gmail.com>
2021-05-16 17:12:34 -0500

    NotFound styles refactor, fonts bug

--- src/components/notFound/NotFound.tsx
+++ src/components/notFound/NotFound.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import './NotFound.scss';
 
-const NotFound: React.FC = () => (
-  <div className="not-found">
-    <h1 className="not-found__text">404</h1>
-  </div>
-)
+
+const NotFound: React.FC = () => {
+
+  const history = useHistory();
+
+  const goBack: React.MouseEventHandler<HTMLAnchorElement> = () => {
+    history.goBack();
+  }
+
+  return (
+    <div className="not-found">
+      <a onClick={ goBack } className="not-found__back">
+        <div className="not-found__back__icon">
+          <div className="arrow arrow__top"></div>
+          <div className="arrow arrow__bottom"></div>
+          <div className="arrow arrow__stick"></div>
+        </div>
+        <span className="not-found__back__text">Go Back</span>
+      </a>
+      <h1 className="not-found__title">404</h1>
+      <span className="not-found__text">The page you're looking for does not exist.</span>
+    </div>
+  )
+}
 
 export default NotFound;
