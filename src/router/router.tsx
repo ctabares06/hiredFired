324cc7a3b43a274906d761e14f4e12da00850790
ctabares06 <ctr9120@gmail.com>
2021-05-19 19:51:24 -0500

    react-transition-group and transitions to the app

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Curriculums from '../components/curriculums/Curriculums';
 import NotFound from '../components/notFound/NotFound';
 import Header from '../components/header/Header';
@@ -10,26 +11,32 @@ import ScrollTop from '../components/scrollTop/ScrollTop';
 
 const Router: React.FC = () => {
 
+  const location = useLocation();
+
   return (
     <>
       <Header />
       <div className="main-content-page">
         <ScrollTop />
         <main className="page-container">
-          <Switch>
-            <Route exact path="/">
-              <Curriculums type="curriculums" />
-            </Route>
-            <Route path="/hired">
-              <Curriculums type="hired" />
-            </Route>
-            <Route path="/fired">
-              <Curriculums type="fired" />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
+          <TransitionGroup>
+            <CSSTransition key={location.key} classNames="page" timeout={300}>
+              <Switch location={location}>
+                <Route exact path="/">
+                  <Curriculums type="curriculums" />
+                </Route>
+                <Route path="/hired">
+                  <Curriculums type="hired" />
+                </Route>
+                <Route path="/fired">
+                  <Curriculums type="fired" />
+                </Route>
+                <Route path="*">
+                  <NotFound />
+                </Route>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
           <EndGameButton />
           <Summary />
           <CurrentGame />
